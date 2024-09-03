4fe210c1c297eb9b3ef7da7f1aeca742c3f7913e
Sai Kiran <neelakantamsaikiran7@gmail.com>
2024-09-03 15:45:00 +0530

    fixed home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './pages/Navbar'
+import Jobs from './pages/Jobs'
+import Services from './pages/Services'
+import ContactUs from './pages/Contactus'
 function App() {
  
 
@@ -10,6 +13,9 @@ function App() {
   <Navbar/>
  <Routes>
   <Route path='/' element={<Home/>}></Route>
+  <Route path='/jobs' element={<Jobs/>}></Route>
+  <Route path='/services' element={<Services/>}></Route>
+  <Route path='/contact' element={<ContactUs/>}></Route>
  </Routes>
   </BrowserRouter>
   </>
