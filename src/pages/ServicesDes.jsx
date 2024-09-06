64147db640e44e0c316154bcb666995c390267e0
Sai Kiran <neelakantamsaikiran7@gmail.com>
2024-09-06 14:40:47 +0530

    impovements

--- src/pages/ServicesDes.jsx
+++ src/pages/ServicesDes.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { useRecoilValue } from 'recoil'
-import { ServicesAtom } from '../store/store'
-
+import React, { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { ServicesAtom } from '../store/store';
 
 export default function ServicesDes() {
-    const service = useRecoilValue(ServicesAtom)
-    console.log(service)
+  const service = useRecoilValue(ServicesAtom);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-950 to-gray-900  font-sora ">
-    <div className="container mx-auto px-6 py-12">
-      <h1 className="font-bold text-center text-3xl md:text-4xl lg:text-5xl mb-4 text-textPrimary animate-fadeIn">
-   {service.services}
-      </h1>
-      <p className='text-center mb-8 animate-fadeIn text-white'>{service.description}</p>
-    
+    <div className="min-h-screen flex flex-col justify-center bg-gradient-to-b from-gray-950 to-gray-900 font-sora">
+      <div className="container mx-auto px-6 py-12">
+        <h1 className="font-bold text-center text-3xl md:text-4xl lg:text-5xl mb-4 text-textPrimary">
+          {service.services}
+        </h1>
+        <p className="text-center mb-8 text-white">
+          {service.description}
+        </p>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
