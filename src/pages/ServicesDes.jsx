95023c8ae644919ead07b53ce89aa1edbbee2fe8
Sai Kiran <neelakantamsaikiran7@gmail.com>
2024-09-06 19:37:14 +0530

    impovements

--- src/pages/ServicesDes.jsx
+++ src/pages/ServicesDes.jsx
@@ -10,12 +10,12 @@ export default function ServicesDes() {
   }, []);
 
   return (
-    <div className="min-h-screen flex flex-col justify-center bg-gradient-to-b from-gray-950 to-gray-900 font-sora">
+    <div className="min-h-screen flex flex-col  bg-gradient-to-b from-gray-950 to-gray-900 font-sora">
       <div className="container mx-auto px-6 py-12">
-        <h1 className="font-bold text-center text-3xl md:text-4xl lg:text-5xl mb-4 text-textPrimary">
+        <h1 className="font-bold  text-3xl md:text-4xl lg:text-5xl mb-4 text-textPrimary">
           {service.services}
         </h1>
-        <p className="text-center mb-8 text-white">
+        <p className="mb-8 text-white">
           {service.description}
         </p>
       </div>
