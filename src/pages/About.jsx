8b38c4634b13d75437363c1285615ee1890d2cdb
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-09-19 13:10:29 +0530

    fixed is

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,7 +39,7 @@ export default function About() {
       <div className='flex flex-col md:flex-row w-full md:space-x-8 space-y-8 md:space-y-0 items-center'>
         <div className='flex flex-col text-md md:text-sm lg:text-2xl items-center md:items-center lg:items-start text-gray-300 font-body md:w-1/2'>
           <p className='text-center md:text-left leading-relaxed mb-4'>
-            Triveda is is more than an agency; it represents the inception of a grand vision. Founded by Vallabhaneni Lakshmi Suresh Chandra and Kosari Shiva Charan, we are dedicated to delivering exceptional services in web development and app development. Our journey commenced with a shared passion for innovation and an unwavering commitment to excellence.
+            Triveda is more than an agency; it represents the inception of a grand vision. Founded by Vallabhaneni Lakshmi Suresh Chandra and Kosari Shiva Charan, we are dedicated to delivering exceptional services in web development and app development. Our journey commenced with a shared passion for innovation and an unwavering commitment to excellence.
           </p>
           <p className='text-center md:text-left leading-relaxed'>
             At Triveda, we believe in transforming ideas into reality and empowering businesses to thrive in the digital world. Our team is propelled by creativity, expertise, and a customer-centric approach, ensuring that each project we undertake becomes a success story. We invite you to join us on this exciting journey as we reshape the digital landscape together.
